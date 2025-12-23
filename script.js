454558872aeb98f59fb87aff018926d4c5e705cc
An <Ans>
2025-12-23 10:03:42 +0700

    change background color, add timeline

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const hamburger = document.getElementById('hamburger');
-    const close = document.getElementById('close');
-    const menuList = document.querySelector('.menu-list')
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const close = document.getElementById("close");
+  const menuList = document.querySelector(".menu-list");
+  const body = document.body;
+  const toggles = document.querySelectorAll(".toggle");
 
-    //Hiển thị hamburger menu
-    hamburger.addEventListener('click', function(){
-        hamburger.style.display = 'none';
-        close.style.display = 'block';
-        menuList.classList.add('menu-open');
-        body.style.overflow = 'hidden';
-    })
+  //Hiển thị hamburger menu
+  hamburger.addEventListener("click", function () {
+    hamburger.style.display = "none";
+    close.style.display = "block";
+    menuList.classList.add("menu-open");
+    body.style.overflow = "hidden";
+  });
 
-    //Ẩn hamburger menu
-    close.addEventListener('click', function(){
-        close.style.display = 'none';
-        hamburger.style.display = '';
-        menuList.classList.remove('menu-open');
-        body.style.overflow = 'auto';
-    })
+  //Ẩn hamburger menu
+  close.addEventListener("click", function () {
+    close.style.display = "none";
+    hamburger.style.display = "";
+    menuList.classList.remove("menu-open");
+    body.style.overflow = "auto";
+  });
 
-    //Đóng menu và reset state
-    function closeMenu() {
-        close.style.display = 'none';
-        hamburger.style.display = '';
-        menuList.classList.remove('menu-open');
-        body.style.overflow = 'auto';
+  //Đóng menu và reset state
+  function closeMenu() {
+    close.style.display = "none";
+    hamburger.style.display = "";
+    menuList.classList.remove("menu-open");
+    body.style.overflow = "auto";
+  }
+
+  // Tự động đóng menu khi màn hình lớn
+  const breakpoint = 990; //CSS breakpoint
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > breakpoint) {
+      // Nếu menu đang mở, tự động đóng
+      if (menuList.classList.contains("menu-open")) {
+        closeMenu();
+      }
     }
+  });
+  toggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      const item = toggle.parentElement;
 
-    // Tự động đóng menu khi màn hình lớn
-    const breakpoint = 990; //CSS breakpoint
-    window.addEventListener('resize', function(){
-        if (window.innerWidth > breakpoint) {
-            // Nếu menu đang mở, tự động đóng
-            if (menuList.classList.contains('menu-open')) {
-                closeMenu();
-            }
-        }
-    })
-})
+      // Toggle active state
+      item.classList.toggle("active");
+    });
+  });
+});
